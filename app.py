6c44243025bc27e082f61fe8a7f1db7e879b600e
Kirt Morgan <kirtmorgan@live.com>
2019-05-08 18:39:23 +0100

    Started a new branch for the homepage and created a new html file called home, also added the relevent code for the page to run in the app.py

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ def story():
     return render_template('story.html')
 
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 if __name__== '__main__':
     app.run(debug=True)
 
