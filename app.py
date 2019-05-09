9055832b5e3ffba114fa4cd94a45ec88ea97bc45
Kirt Morgan <kirtmorgan@live.com>
2019-05-09 20:00:27 +0100

    Added the app route into app.py for the articles page

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from articles_db import articles
+
 app = Flask(__name__)
 
 @app.route('/story')
 
 def story():
     return render_template('story.html')
-
 
 @app.route('/home')
 def home():
@@ -14,6 +15,10 @@ def home():
 @app.route('/contacts')
 def contacts():
     return render_template('contacts.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles_data = articles)
 
 if __name__== '__main__':
     app.run(debug=True)
