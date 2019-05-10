cd9a3d8b6ccf4593a7b80188dff908d7329a79f9
Tahsin Khan <tahsin_k@live.co.uk>
2019-05-10 15:29:46 +0100

    started article

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-from article_chef_select import chef_pick
+from article import chef_pick
 
 app = Flask(__name__)
 
 @app.route('/story')
-
 def story():
     return render_template('story.html')
 
@@ -16,6 +15,9 @@ def home():
 def contacts():
     return render_template('contacts.html')
 
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
 
 if __name__== '__main__':
     app.run(debug=True)
