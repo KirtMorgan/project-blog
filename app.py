4794a5e2a80496478fad59c31d5ace5b509dd6a2
Yousuf Ali <yousuf-123@hotmail.co.uk>
2019-05-10 15:14:39 +0100

    added functions that pass in dictionary id to get correct info

--- app.py
+++ app.py
@@ -25,17 +25,17 @@ def articles():
     return render_template('articles.html', articles_data_db = articles_with_data)
 
 
-def show_article(id):
-    if int(id) > len(articles):
+def show_article(dict_id):
+    if int(dict_id) > len(articles):
         return 'article doesnt exist'
     else:
-        article = articles[int(id)]
+        article = articles[int(dict_id)]
         return article
 
 @app.route('/articles/<string:id>')
-def show_article_page(id):
-    result = show_article(id)
-    return render_template('display_article_page.html', id=id, article=result)
+def show_article_page(dict_id):
+    result = show_article(dict_id)
+    return render_template('display_article_page.html', dict_id=dict_id, article=result)
 
 
 if __name__== '__main__':
