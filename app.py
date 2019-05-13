2d6510dbb7de6f141d452116c26e95a8c1800a48
Tahsin Khan <tahsin_k@live.co.uk>
2019-05-13 09:25:03 +0100

    most recent update

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from articles import articles
-from article_sql_real import article_db
+from article_sql_real import *
 
 # app is the instance of the Flask object that has a method
 app = Flask(__name__)
 
 # meothod calling the articles db
 articles_with_data = articles()
-articles_from_sql = article_db()
+articles_from_sql = article_sql_db()
 
 @app.route('/story')
 def story():
@@ -40,6 +40,7 @@ def show_article_page(dict_id):
 if __name__== '__main__':
     app.run(debug=True)
 
-@app.route('/sql-articles')
+#sql db route
+@app.route('/sqlarticles')
 def sql_articles():
     return render_template('articles_sql.html', article_results=articles_from_sql)
--- article_sql_real.py
+++ article_sql_real.py
@@ -1,14 +1,7 @@
 from sql_db_connection import *
 
-def article_db():
+def article_sql_db():
     query = 'SELECT * FROM ArticlesData;'
     cursor.execute(query)
     article_results = cursor.fetchall()
     return article_results
-
-# hi = {}
-# red = []
-# for article in article_results:
-#     red.append(article)
-#
-# print(red)
